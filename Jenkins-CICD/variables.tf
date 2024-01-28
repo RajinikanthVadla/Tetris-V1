749b635a9607bd70cad1ec2899d6753a10604a7a
RajinikanthVadla <157952937+RajinikanthVadla@users.noreply.github.com>
2024-01-28 17:45:49 +0530

    Update variables.tf

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-southeast-2"
+  default     = "ap-south-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "neflix-demo"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,19 +12,19 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-03f4878755434977f"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "purplehaze777"
+  default     = "rajinikanths3"
 }
 
-variable "dynamodb_table" {
-  description = "The name of the dynamodb table"
-  type        = string
-  default     = "purplehaze777-dynamodb-table"
-}
+#variable "dynamodb_table" {
+ # description = "The name of the dynamodb table"
+  #type        = string
+  #default     = "purplehaze777-dynamodb-table"
+#}
 
 variable "acl" {
   description = "The ACL (Access Control List) for the S3 bucket"
