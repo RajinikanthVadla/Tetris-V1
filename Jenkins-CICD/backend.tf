b0cc3ce4027e27a4888ba1a5cec44db4226fd1c4
RajinikanthVadla <157952937+RajinikanthVadla@users.noreply.github.com>
2024-01-28 18:39:09 +0530

    Update backend.tf

--- Jenkins-CICD/backend.tf
+++ Jenkins-CICD/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = var.bucket_name
-    key            = "my-terraform-environment/main"
+    key            = "my-terraform-environment/main/terraform.tfstate"
     region         = var.aws_region
     #dynamodb_table = var.dynamodb_table
   }
