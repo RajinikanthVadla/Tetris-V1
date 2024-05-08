554b38f8743785395624a235bc51b39e39d8757f
Rajinikanth Vadla <157952937+RajinikanthVadla@users.noreply.github.com>
2024-05-08 08:23:47 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajinikanths3" # Replace with your actual S3 bucket name
+    bucket = "rajinikanths32024" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
